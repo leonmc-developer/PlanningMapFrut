ba7d54f3e7bdf23d1a9b47621557419b96737c40
adan.mariscal <adanmariscalcaballero@gmail.com>
2025-03-20 11:58:19 -0400

    fix menu url select part.2

--- app/Views/template/tp_header.php
+++ app/Views/template/tp_header.php
@@ -170,7 +170,7 @@
           <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
             <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pages</a></li>
             <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Dashboard</li>
-            <li class="breadcrumb-item text-sm text-dark active" aria-current="page"><?=current_url();?></li>
+            <!-- <li class="breadcrumb-item text-sm text-dark active" aria-current="page"><?=current_url();?></li> -->
           </ol>
         </nav>
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
