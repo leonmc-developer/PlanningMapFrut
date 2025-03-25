80780b24feeef53ce6959370450ac6a05be56505
leonmc developer <leonmc.developer@gmail.com>
2025-03-24 21:53:27 -0400

    fix init details-terreno de bd

--- db_planing_mapfrut.sql
+++ db_planing_mapfrut.sql
@@ -1,13 +1,34 @@
--- Adminer 5.0.6 MariaDB 10.4.24-MariaDB dump
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 25-03-2025 a las 02:51:32
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.4
 
-SET NAMES utf8;
-SET time_zone = '+00:00';
-SET foreign_key_checks = 0;
-SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-DROP TABLE IF EXISTS `combustibles`;
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `db_planing_mapfrut`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `combustibles`
+--
+
 CREATE TABLE `combustibles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `fecha` datetime NOT NULL,
   `tipo` int(11) NOT NULL,
   `cantidad` int(11) NOT NULL,
@@ -15,14 +36,37 @@ CREATE TABLE `combustibles` (
   `responsable` int(11) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `deleted_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `deleted_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `herramientas`;
+--
+-- Estructura de tabla para la tabla `groups`
+--
+
+CREATE TABLE `groups` (
+  `id` mediumint(8) UNSIGNED NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `description` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `groups`
+--
+
+INSERT INTO `groups` (`id`, `name`, `description`) VALUES
+(1, 'admin', 'Administrator'),
+(2, 'members', 'General User');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `herramientas`
+--
+
 CREATE TABLE `herramientas` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `codigo` varchar(10) NOT NULL,
   `equipo` int(11) NOT NULL,
   `marca` int(11) NOT NULL,
@@ -37,36 +81,45 @@ CREATE TABLE `herramientas` (
   `accion_tomada` varchar(250) DEFAULT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `deleted_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `deleted_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `hileras`;
+--
+-- Estructura de tabla para la tabla `hileras`
+--
+
 CREATE TABLE `hileras` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `tag` varchar(25) NOT NULL,
   `color` varchar(7) NOT NULL,
   `tipo_linea` tinyint(4) NOT NULL,
   `estado` tinyint(4) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `updated_at` datetime DEFAULT NULL ON UPDATE current_timestamp(),
-  `deleted_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `deleted_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `hilera_persona`;
+--
+-- Estructura de tabla para la tabla `hilera_persona`
+--
+
 CREATE TABLE `hilera_persona` (
   `hilera_id` int(11) NOT NULL,
-  `planta_id` int(11) NOT NULL,
-  KEY `hilera_id` (`hilera_id`,`planta_id`)
+  `planta_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `insumos`;
+--
+-- Estructura de tabla para la tabla `insumos`
+--
+
 CREATE TABLE `insumos` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `codigo` varchar(10) NOT NULL,
   `insumo` int(11) NOT NULL,
   `marca` int(11) DEFAULT NULL,
@@ -81,14 +134,30 @@ CREATE TABLE `insumos` (
   `accion_tomada` varchar(250) DEFAULT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `deleted_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `deleted_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `materiales`;
+--
+-- Estructura de tabla para la tabla `login_attempts`
+--
+
+CREATE TABLE `login_attempts` (
+  `id` mediumint(8) UNSIGNED NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `login` varchar(100) DEFAULT NULL,
+  `time` int(11) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `materiales`
+--
+
 CREATE TABLE `materiales` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `codigo` int(11) NOT NULL,
   `recurso` int(11) NOT NULL,
   `cantidad` int(11) NOT NULL,
@@ -103,14 +172,40 @@ CREATE TABLE `materiales` (
   `accion_tomada` varchar(250) DEFAULT NULL,
   `created_at` datetime DEFAULT current_timestamp(),
   `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `deleted_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `deleted_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `plantas`;
+--
+-- Estructura de tabla para la tabla `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `version` varchar(255) NOT NULL,
+  `class` varchar(255) NOT NULL,
+  `group` varchar(255) NOT NULL,
+  `namespace` varchar(255) NOT NULL,
+  `time` int(11) NOT NULL,
+  `batch` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `version`, `class`, `group`, `namespace`, `time`, `batch`) VALUES
+(1, '20181211100537', 'IonAuth\\Database\\Migrations\\Migration_Install_ion_auth', 'default', 'IonAuth', 1742607293, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `plantas`
+--
+
 CREATE TABLE `plantas` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `tag` varchar(25) NOT NULL,
   `tipo_planta_id` int(11) NOT NULL,
   `fecha_plantacion` datetime NOT NULL,
@@ -121,14 +216,17 @@ CREATE TABLE `plantas` (
   `estado` tinyint(4) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `updated_at` datetime DEFAULT NULL ON UPDATE current_timestamp(),
-  `delete_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `delete_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `sectores`;
+--
+-- Estructura de tabla para la tabla `sectores`
+--
+
 CREATE TABLE `sectores` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `nombre` int(11) NOT NULL,
   `poligono` varchar(255) NOT NULL,
   `descripcion` varchar(255) NOT NULL,
@@ -136,32 +234,50 @@ CREATE TABLE `sectores` (
   `estado` tinyint(4) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `updated_at` datetime DEFAULT NULL ON UPDATE current_timestamp(),
-  `deleted_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `deleted_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `terrenos`;
+--
+-- Estructura de tabla para la tabla `terrenos`
+--
+
 CREATE TABLE `terrenos` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `nombre` varchar(100) NOT NULL,
+  `lema` varchar(50) DEFAULT NULL,
   `descripcion` text NOT NULL,
-  `coordenadas` text NOT NULL,
+  `propietario` varchar(50) NOT NULL,
+  `ubicacion` varchar(75) DEFAULT NULL,
+  `superficie` int(11) NOT NULL,
+  `coordenadas` varchar(25) DEFAULT NULL,
+  `altitud` int(11) DEFAULT NULL,
+  `tipo_suelo` varchar(50) NOT NULL,
+  `poligono` text NOT NULL,
   `color` varchar(7) DEFAULT NULL,
   `relleno` varchar(7) DEFAULT NULL,
   `estado` tinyint(4) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `deleted_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `deleted_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `terrenos` (`id`, `nombre`, `descripcion`, `coordenadas`, `color`, `relleno`, `estado`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1,	'achiras',	'achiras',	'[{\"lat\":-18.163279331289505,\"lng\":-63.803298868926994},{\"lat\":-18.16377810001784,\"lng\":-63.80311044370675},{\"lat\":-18.16369283469969,\"lng\":-63.80344974315759},{\"lat\":-18.163508064443985,\"lng\":-63.80359793521025}]',	'achiras',	'achiras',	0,	'2025-03-09 00:00:00',	'2025-03-10 18:04:26',	NULL);
+--
+-- Volcado de datos para la tabla `terrenos`
+--
 
-DROP TABLE IF EXISTS `tipo_plantas`;
+INSERT INTO `terrenos` (`id`, `nombre`, `lema`, `descripcion`, `propietario`, `ubicacion`, `superficie`, `coordenadas`, `altitud`, `tipo_suelo`, `poligono`, `color`, `relleno`, `estado`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'ACHIRAS-CABAÑITA', 'Cabaña / Huerta', 'Somos una familia dedicada a cultivar frutas frescas , ubicada en la localiad de Achiras, Santa Cruz - Bolivia sino también la de nuestra comunidad. Nos apasiona compartir lo que producimos con nuestros amigos y vecinos, y creemos en el valor de la agricultura sustentable para cuidar el medio ambiente y promover una alimentación sana.. ', 'Dolly Caballero Pinto', 'Achiras - Samaipata', 1800, '(454545.22,4587451.23)', 980, 'Franco Arcilloso', '[{\"lat\":-18.163279331289505,\"lng\":-63.803298868926994},{\"lat\":-18.16377810001784,\"lng\":-63.80311044370675},{\"lat\":-18.16369283469969,\"lng\":-63.80344974315759},{\"lat\":-18.163508064443985,\"lng\":-63.80359793521025}]', 'achiras', 'achiras', 0, '2025-03-09 00:00:00', '2025-03-24 21:47:22', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_plantas`
+--
+
 CREATE TABLE `tipo_plantas` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `nombre` varchar(75) NOT NULL,
   `variedad` varchar(100) NOT NULL,
   `descripcion` text DEFAULT NULL,
@@ -170,24 +286,301 @@ CREATE TABLE `tipo_plantas` (
   `imagen` varchar(25) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `updated_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `delete_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `delete_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `tipo_plantas` (`id`, `nombre`, `variedad`, `descripcion`, `color`, `fondo`, `imagen`, `created_at`, `updated_at`, `delete_at`) VALUES
-(36,	'lima',	'lima',	'lima',	'',	'',	'',	'2025-03-14 00:30:30',	'2025-03-14 00:30:30',	NULL),
-(37,	'mandarina',	'mandarina',	'mandarina',	'',	'',	'',	'2025-03-15 02:06:14',	'2025-03-15 02:06:14',	NULL);
+--
+-- Volcado de datos para la tabla `tipo_plantas`
+--
 
-DROP TABLE IF EXISTS `tipo_recursos_inventario`;
+INSERT INTO `tipo_plantas` (`id`, `nombre`, `variedad`, `descripcion`, `color`, `fondo`, `imagen`, `created_at`, `updated_at`, `delete_at`) VALUES
+(36, 'lima', 'lima', 'lima', '', '', '', '2025-03-14 00:30:30', '2025-03-14 00:30:30', NULL),
+(37, 'mandarina', 'mandarina', 'mandarina', '', '', '', '2025-03-15 02:06:14', '2025-03-15 02:06:14', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_recursos_inventario`
+--
+
 CREATE TABLE `tipo_recursos_inventario` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `nombre` varchar(100) NOT NULL,
   `tipo` int(11) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `deleted_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `deleted_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
--- 2025-03-21 01:19:52 UTC
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` mediumint(8) UNSIGNED NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(254) NOT NULL,
+  `activation_selector` varchar(255) DEFAULT NULL,
+  `activation_code` varchar(255) DEFAULT NULL,
+  `forgotten_password_selector` varchar(255) DEFAULT NULL,
+  `forgotten_password_code` varchar(255) DEFAULT NULL,
+  `forgotten_password_time` int(11) UNSIGNED DEFAULT NULL,
+  `remember_selector` varchar(255) DEFAULT NULL,
+  `remember_code` varchar(255) DEFAULT NULL,
+  `created_on` int(11) UNSIGNED NOT NULL,
+  `last_login` int(11) UNSIGNED DEFAULT NULL,
+  `active` tinyint(1) UNSIGNED DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `company` varchar(100) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `terreno_id` int(11) DEFAULT NULL,
+  `terreno_name` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `ip_address`, `username`, `password`, `email`, `activation_selector`, `activation_code`, `forgotten_password_selector`, `forgotten_password_code`, `forgotten_password_time`, `remember_selector`, `remember_code`, `created_on`, `last_login`, `active`, `first_name`, `last_name`, `company`, `phone`, `terreno_id`, `terreno_name`) VALUES
+(1, '127.0.0.1', 'administrator', '$2y$12$bijOVHiW4hJ5lGlv3l9qdetU5WWtK8JXZCK2VuitJCIfXgnwS6zb2', 'admin@admin.com', NULL, '', NULL, NULL, NULL, 'feb211da87e8aefba558e27c18cdd2c925a8f935', '$2y$10$DWfsJd3MaFtQkFwU85WwQeU90tR.SpK3fYT9sUdaCcw166Y8JAc8S', 1268889823, 1742864887, 1, 'Admin', 'istrator', 'ADMIN', '0', 1, 'ACHIRAS-CABAÑITA ');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users_groups`
+--
+
+CREATE TABLE `users_groups` (
+  `id` mediumint(8) UNSIGNED NOT NULL,
+  `user_id` mediumint(8) UNSIGNED NOT NULL,
+  `group_id` mediumint(8) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `users_groups`
+--
+
+INSERT INTO `users_groups` (`id`, `user_id`, `group_id`) VALUES
+(1, 1, 1),
+(2, 1, 2);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `combustibles`
+--
+ALTER TABLE `combustibles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `groups`
+--
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `herramientas`
+--
+ALTER TABLE `herramientas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `hileras`
+--
+ALTER TABLE `hileras`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `hilera_persona`
+--
+ALTER TABLE `hilera_persona`
+  ADD KEY `hilera_id` (`hilera_id`,`planta_id`);
+
+--
+-- Indices de la tabla `insumos`
+--
+ALTER TABLE `insumos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `login_attempts`
+--
+ALTER TABLE `login_attempts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `materiales`
+--
+ALTER TABLE `materiales`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `plantas`
+--
+ALTER TABLE `plantas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `sectores`
+--
+ALTER TABLE `sectores`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `terrenos`
+--
+ALTER TABLE `terrenos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `tipo_plantas`
+--
+ALTER TABLE `tipo_plantas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `tipo_recursos_inventario`
+--
+ALTER TABLE `tipo_recursos_inventario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `activation_selector` (`activation_selector`),
+  ADD UNIQUE KEY `forgotten_password_selector` (`forgotten_password_selector`),
+  ADD UNIQUE KEY `remember_selector` (`remember_selector`);
+
+--
+-- Indices de la tabla `users_groups`
+--
+ALTER TABLE `users_groups`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `users_groups_user_id_foreign` (`user_id`),
+  ADD KEY `users_groups_group_id_foreign` (`group_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `combustibles`
+--
+ALTER TABLE `combustibles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `groups`
+--
+ALTER TABLE `groups`
+  MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `herramientas`
+--
+ALTER TABLE `herramientas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `hileras`
+--
+ALTER TABLE `hileras`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `insumos`
+--
+ALTER TABLE `insumos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `login_attempts`
+--
+ALTER TABLE `login_attempts`
+  MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `materiales`
+--
+ALTER TABLE `materiales`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `plantas`
+--
+ALTER TABLE `plantas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `sectores`
+--
+ALTER TABLE `sectores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `terrenos`
+--
+ALTER TABLE `terrenos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo_plantas`
+--
+ALTER TABLE `tipo_plantas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo_recursos_inventario`
+--
+ALTER TABLE `tipo_recursos_inventario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `users_groups`
+--
+ALTER TABLE `users_groups`
+  MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `users_groups`
+--
+ALTER TABLE `users_groups`
+  ADD CONSTRAINT `users_groups_group_id_foreign` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `users_groups_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
